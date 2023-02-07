c9eefa384012d0d528285bee4b504d9bc7634754
bayraa11234 <odbayardorjsuren8@gmail.com>
2023-02-07 11:10:44 +0800

    home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Carousell from "./components/Section/Carousel";
 import Section from "./components/Section/Section";
+import BigSell from "./aside/BigSell";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="container">
         <Section />
         <Carousell />
+        {/* <BigSell/> */}
       </div>
       <h1>hello</h1>
     </>
