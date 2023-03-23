eb099dfb6943b21782787d9311fb5079705b4ab9
odbayar <odbayardorjsuren8@gmail.com>
2023-03-23 19:55:37 +0800

    plus

--- src/components/Detail/DetailProduct.jsx
+++ src/components/Detail/DetailProduct.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
 export default function DetailProduct() {
+  const [count, setCount] = useState(1);
   return (
-    <div className="d-flex" style={{ gap: 15 }}>
-      <div>
-        <img src="./detailProduct/bidPlay.png" alt="" />
-        <div className="d-flex my-3" style={{ gap: 15 }}>
-          <img src="./detailProduct/Play.png" alt="" />
-          <img src="./detailProduct/Play.png" alt="" />
+    <div className=" row d-flex">
+      <div className="col-6">
+        <img src="./detailProduct/bidPlay.png" alt="" className="img-fluid" />
+        <div className="d-flex my-3 gap-2 ">
+          <img src="./detailProduct/Play.png" alt="" className="w-50" />
+          <img src="./detailProduct/Play.png" alt="" className="w-50" />
         </div>
       </div>
-      <div>
+      <div className="col-6">
         <div style={{ borderBottom: "1px solid black" }}>
           <h2>Play game</h2>
           <div>$11,70</div>
@@ -55,6 +57,53 @@ export default function DetailProduct() {
                 />
               </div>
             </span>
+          </div>
+          <div className="d-flex align-items-center my-3">
+            Size:{" "}
+            <div>
+              <button type="button" className="btn btn-secondary mx-2">
+                30
+              </button>
+              <button type="button" className="btn btn-secondary ">
+                56
+              </button>
+              <button type="button" className="btn btn-secondary mx-2">
+                42
+              </button>
+              <button className="btn btn-secondary ">48</button>
+            </div>
+          </div>
+          <div className="d-flex align-items-center">
+            Quality:{" "}
+            <div className="d-flex align-items-center border border-dark rounded-3">
+              <button
+                className="btn btn-secondary "
+                onClick={() => {
+                  setCount(count - 1);
+                }}
+              >
+                -
+              </button>
+              <div className="mx-3">{count}</div>
+              <button
+                className="btn btn-secondary "
+                onClick={() => {
+                  setCount(count + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="d-flex gap-3 my-3">
+            <button className="btn bg-warning">Add to card</button>
+            <button className=" btn bg-warning">Buy it now</button>
+
+            <img
+              src="./detailProduct/favourite.png"
+              alt=""
+              style={{ maxWidth: "25px" }}
+            />
           </div>
         </div>
       </div>
